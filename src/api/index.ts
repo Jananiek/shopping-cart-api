ba2f3948f695da697ab4ee8a101aaee4c32e52bf
jananie-wathsala <jananie.kum@gmail.com>
2022-11-14 09:40:16 +0530

    add swagger docmentation & ReadMe

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,11 @@ import health from './routes/health';
 import auth from './routes/auth';
 import product from './routes/product';
 import order from './routes/order';
+import swaggerDoc from './routes/swaggerDoc';
 
 export default () => {
   const app = Router();
+  swaggerDoc(app);
   health(app);
   auth(app);
   product(app);
