6c9d5ce053f5a39edc3adc01beaa9a73fe8ee3ac
Jananie Wathsala <jananie.kum@gmail.com>
2022-11-14 09:09:38 +0800

    add auth headers and refactor erorr messages (#8)

--- src/middleware/auth/passportConfig.ts
+++ src/middleware/auth/passportConfig.ts
@@ -3,7 +3,11 @@ import { UserService } from "../../modules/user/services";
 import { matchPassword } from "../../utils/helper";
 
 const LocalStrategy = require('passport-local');
+const passportJWT = require("passport-jwt");
 const userService = new UserService();
+
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 
 
@@ -13,18 +17,18 @@ module.exports = (passport) => {
         new LocalStrategy(
             {
                 usernameField: "email",
-                passwordField: "password",
+                passwordField: "password"
             },
             async (email, password, done) => {
                 try {
                     const userExists = await userService.emailExists(email)
 
                     if (userExists) {
-                        return done(null, false);
+                        return done(null, false, { message: 'User already exists' });
                     }
 
-                    const user = await userService.createUser({ email, password, mobile: '123456789' });
-                    return done(null, user);
+                    const user = await userService.createUser({ email, password, mobile: '0123456789' });
+                    return done(null, user, { message: 'User registered successfully' });
                 } catch (error) {
                     done(error);
                 }
@@ -42,10 +46,27 @@ module.exports = (passport) => {
             async (email, password, done) => {
                 try {
                     const user = await userService.emailExists(email);
-                    if (!user) return done(null, false);
+                    if (!user) return done(null, false, { message: 'User Not found' });
                     const isMatch = await matchPassword(password, user.password);
-                    if (!isMatch) return done(null, false);
-                    return done(null, { id: user.id, email: user.email });
+                    if (!isMatch) return done(null, false, { message: 'Password does not match' });
+                    return done(null, { id: user.id, email: user.email }, { message: 'Logged in Successfully' });
+                } catch (error) {
+                    return done(error, false);
+                }
+            }
+        )
+    );
+
+    passport.use(
+        "jwt",
+        new JWTStrategy({
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'jwt_secret'
+        },
+            async (jwtPayload, done) => {
+                try {
+                    if (!jwtPayload) return done(null, false);
+                    return done(null, { userId: jwtPayload.id });
                 } catch (error) {
                     return done(error, false);
                 }
